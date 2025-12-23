55d23cc5327642d877d2dc4f7b61386a7687befe
Tanakorn Kaewmai <tanakorn488@outlook.com>
2025-12-23 16:20:51 +0700

    Update

--- backend/rice_core/settings.py
+++ backend/rice_core/settings.py
@@ -92,3 +92,8 @@ CSRF_TRUSTED_ORIGINS = [
     "https://riceproject-production.up.railway.app",
     
 ]
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SAMESITE = 'None'
